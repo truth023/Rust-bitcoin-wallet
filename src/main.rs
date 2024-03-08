fea6606cb5ffe444ad71d69624f6dd31e4408901
techtruth1 <techtrust111@gmail.com>
2024-03-08 06:29:47 -0800

    feat:main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+
 use anyhow::Result;
 mod eth_wallet;
 mod utils;
@@ -16,11 +17,7 @@ async fn main() -> Result<()> {
     let pub_address = eth_wallet::public_key_address(&pub_key);
     println!("public address: {:?}", pub_address);
 
-    // let crypto_wallet = eth_wallet::Wallet::new(&secret_key, &pub_key);
-    // println!("crypto_wallet: {:?}", &crypto_wallet);
-
     let wallet_file_path = "crypto_wallet.json";
-    // crypto_wallet.save_to_file(wallet_file_path)?;
 
     let loaded_wallet = eth_wallet::Wallet::from_file(wallet_file_path)?;
     println!("loaded_wallet: {:?}", loaded_wallet);
